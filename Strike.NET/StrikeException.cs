06462c65e7ac082a715c34eec274340bdb18ad89
Nate Shoffner <nate.shoffner@gmail.com>
2015-03-24 05:10:07 -0400

    Added StrikeException

---
+++ Strike.NET/StrikeException.cs
@@ -0,0 +1,37 @@
+﻿#region
+
+using System;
+using System.Runtime.Serialization;
+
+#endregion
+
+namespace StrikeNET
+{
+    [Serializable]
+    public class StrikeException : Exception
+    {
+        public StrikeException()
+        {
+        }
+
+        public StrikeException(string message) : base(message)
+        {
+        }
+
+        public StrikeException(string format, params object[] args) : base(string.Format(format, args))
+        {
+        }
+
+        public StrikeException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public StrikeException(string format, Exception innerException, params object[] args) : base(string.Format(format, args), innerException)
+        {
+        }
+
+        protected StrikeException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
